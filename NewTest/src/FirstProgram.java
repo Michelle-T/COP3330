6cb478d684b56287d4608df5f66d51defbd62cf0
Michelle T <42843557+Michelle-T@users.noreply.github.com>
2020-01-16 16:36:55 -0500

    enter

---
+++ NewTest/src/FirstProgram.java
@@ -0,0 +1,17 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int age = 22; 
+		double weight = 1.234567890;
+		System.out.printf("Hello people!\nI am %d years old!%nI weight %.2f\n", age, weight);
+		
+		int x = 12;
+		int y = 10;
+		double u = 10.0;
+		System.out.println(x / 0.0001);
+		System.out.println(x / u);
+		System.out.println(x % y);
+	}
+
+}
